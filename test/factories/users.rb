c7560a3deb4c3e6d43f015a1d426400c9d7f6ca4
zemskov.va <vladimir.zemskov@dualbootpartners.com>
2021-07-13 08:51:33 +0300

    rubocop fix

--- test/factories/users.rb
+++ test/factories/users.rb
@@ -2,21 +2,21 @@ FactoryBot.define do
   factory :user do
     first_name
     last_name
-    password 
-    email { "MyString" }
-    avatar { "MyString" }
-    type { "" }
+    password
+    email { 'MyString' }
+    avatar { 'MyString' }
+    type { '' }
 
     factory :developer do
-      type { "Developer" }
+      type { 'Developer' }
     end
 
     factory :manager do
-      type { "Manager" }
+      type { 'Manager' }
     end
 
     factory :admin do
-      type { "Admin" }
+      type { 'Admin' }
     end
   end
 end
