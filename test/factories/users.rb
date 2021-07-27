c030aa407143c93cbcb52c0caa6e0521b5936503
zemskov.va <vladimir.zemskov@dualbootpartners.com>
2021-07-27 18:01:00 +0300

    fix error messages

--- test/factories/users.rb
+++ test/factories/users.rb
@@ -3,23 +3,20 @@ FactoryBot.define do
     first_name
     last_name
     password
-    email { 'MyString@mail.ru' }
+    email
     avatar { 'MyString' }
     type { '' }
 
     factory :developer do
       type { 'Developer' }
-      email { 'MyString1@mail.ru' }
     end
 
     factory :manager do
       type { 'Manager' }
-      email { 'MyString2@mail.ru' }
     end
 
     factory :admin do
       type { 'Admin' }
-      email { 'MyString3@mail.ru' }
     end
   end
 end
