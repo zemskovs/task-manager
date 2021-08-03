81dfb1cf99e81ed33b91367f9e0eb9319895245c
zemskov.va <vladimir.zemskov@dualbootpartners.com>
2021-08-03 08:25:15 +0300

    add auth for board

--- test/controllers/web/boards_controller_test.rb
+++ test/controllers/web/boards_controller_test.rb
@@ -1,6 +1,11 @@
 require 'test_helper'
 
 class Web::BoardsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    user = create(:user)
+    sign_in user
+  end
+
   test 'should get show' do
     get web_boards_show_url
     assert_response :success
