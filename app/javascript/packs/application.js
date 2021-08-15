1073d809376252886e2619c24b45ea92d3077fa3
zemskov.va <nikzevs@gmail.com>
2021-08-15 20:11:29 +0300

    add kanban react

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,7 +3,11 @@ import Turbolinks from 'turbolinks';
 import * as ActiveStorage from '@rails/activestorage';
 import 'channels';
 import 'material-design-lite/material.js';
+import WebpackerReact from 'webpacker-react';
+import TaskBoard from './components/TaskBoard';
 
 Rails.start();
 Turbolinks.start();
 ActiveStorage.start();
+
+WebpackerReact.setup({ TaskBoard });
