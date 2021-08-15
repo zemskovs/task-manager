1855384a695b012889327360acbb0c7a6e2b258a
zemskov.va <nikzevs@gmail.com>
2021-08-15 19:50:36 +0300

    add tasks api

--- test/factories/tasks.rb
+++ test/factories/tasks.rb
@@ -1,10 +1,8 @@
 FactoryBot.define do
   factory :task do
-    name
-    description
-    author factory: :manager
-    assignee_id factory: :developer
-    state { 'new_task' }
-    expired_at { '2021-07-03' }
+    name { generate :string }
+    description { generate :string }
+    author_id { create :user }
+    assignee_id { create :user }
   end
 end
